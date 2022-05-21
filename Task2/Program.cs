be005a04b4a98b4016dc378d3c062ad83be4ba37
edochik <edoch@yandex.ru>
2022-05-21 23:01:02 +0300

    add informatino in Task1|2|3|

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+2. В двумерном массиве целых чисел. 
+Удалить строку и столбец, на пересечении которых расположен наименьший элемент.
+*/
+
+int[,] NewMatrix(int[,] matrix)//создать массив
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 99);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)// печать массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($" {matrix[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+(int, int) FindSmallIndex(int[,] array)//находит наименьший элемент в массиве
+{
+    int min = array[0, 0];
+    int row = 0;
+    int column = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (min > array[i, j])
+            {
+                min = array[i, j];
+                row = i;
+                column = j;
+            }
+        }
+    }
+    return (row, column);
+}
+
+void Remove(int[,] array, int row, int column) // должен удалять строчку и столбец
+{
+    int[,] newArray = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            newArray[i, j] = array[i, j];
+        }
+    }
+    for (int i = row + 1; i < array.GetLength(0); i++)
+    {
+        for (int j = column + 1; j < array.GetLength(1); j++)
+        {
+            newArray[i - 1, j - 1] = array[i, j];
+
+        }
+    }
+    array = newArray;
+}
+
+int[,] array = new int[3, 3];
+NewMatrix(array);
+PrintMatrix(array);
+Console.WriteLine(FindSmallIndex(array));
+Console.WriteLine();
+Remove(array, 2, 2);
+PrintMatrix(array);
+
+//Console.Write(FindSmallIndex(array));
+
+/*
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+3. Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы 
+соответствующего элемента
+*/
+
+int[,,] NewMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                matrix[i, j, z] = new Random().Next(0, 9);
+            }
+
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                Console.Write($" {matrix[i, j, z]}");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] matrix = new int[5, 5, 5];
+NewMatrix(matrix);
+PrintMatrix(matrix);
+
+
+
+
+//https://www.youtube.com/watch?v=NLu9y4LKXQ0
+
+
+/*
+//задача из интернета распечатать определенный узор в двухмерном массиве
+int[,] CreateMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j) matrix[i, j] = 1;
+            else if (i == (matrix.GetLength(1) - 1) - j) matrix[i, j] = 1;
+            else matrix[i, j] = 0;
+        }
+    }
+    return matrix;
+}
+
+
+
+int[,] matrix = new int[7, 7];
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($" {matrix[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintMatrix(CreateMatrix(matrix));
+*/
+
+
