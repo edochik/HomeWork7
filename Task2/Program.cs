830ead8d4a6caf5668142d9486b65147968afe3d
edochik <edoch@yandex.ru>
2022-05-23 22:33:05 +0300

    add Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -27,6 +27,61 @@ void PrintMatrix(int[,] matrix)// печать массива
     }
 }
 
+
+int[,] Remove(int[,] array) // должен удалять строчку и столбец
+{
+    int min = array[0, 0];
+    int row = 0;
+    int column = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (min > array[i, j])
+            {
+                min = array[i, j];
+                row = i;
+                column = j;
+            }
+        }
+    }
+
+    int[,] newArray = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];// создаем новый массив меньше на 1 строчку и 1 колонку
+
+    for (int i = 0, j = 0; i < array.GetLength(0); i++)
+    {
+        if (i == row)
+            continue;
+
+        for (int k = 0, u = 0; k < array.GetLength(1); k++)
+        {
+            if (k == column)
+                continue;
+
+            newArray[j, u] = array[i, k];
+            u++;
+        }
+        j++;
+    }
+
+    return newArray;
+}
+int[,] array = new int[4, 4];
+NewMatrix(array)
+PrintMatrix(array);
+Console.WriteLine();
+var trim = Remove(array);
+
+PrintMatrix(trim);
+
+
+
+
+
+
+
+// нахождение минимального элемента и вывод двух значений
+/*
 (int, int) FindSmallIndex(int[,] array)//находит наименьший элемент в массиве
 {
     int min = array[0, 0];
@@ -46,37 +101,4 @@ void PrintMatrix(int[,] matrix)// печать массива
     }
     return (row, column);
 }
-
-void Remove(int[,] array, int row, int column) // должен удалять строчку и столбец
-{
-    int[,] newArray = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];// создаем новый массив меньше на 1 строчку и 1 колонку
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < column; j++)
-        {
-            newArray[i, j] = array[i, j];
-        }
-    }
-    for (int i = row + 1; i < array.GetLength(0); i++)
-    {
-        for (int j = column + 1; j < array.GetLength(1); j++)
-        {
-            newArray[i - 1, j - 1] = array[i, j];
-        }
-    }
-    array = newArray;
-}
-
-int[,] array = new int[4, 4];
-NewMatrix(array);
-PrintMatrix(array);
-Console.WriteLine(FindSmallIndex(array));
-Console.WriteLine();
-Remove(array, 3, 3);
-PrintMatrix(array);
-
-/*
-Вячеслав, добрый вечер. Спасибо за обратную связь. 
-1. ДЗ по спирали, пытался сделать сам, но все равно не до конца понимаю. Но смог сделал благодаря уроку на youtube.
-2. ДЗ 
 */
